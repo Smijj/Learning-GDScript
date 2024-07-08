9a6d61f8e9ba72de3cadd0651acc738bafe40dc2
Declan Smyth <declan.smyth@hotmail.com>
2024-07-08 20:56:00 +0930

    Hello World

---
+++ GD.Learning GDScript/DSmyth/Scripts/main.gd
@@ -0,0 +1,7 @@
+extends Node
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$Label.text = "Hello World"
+	$Label.modulate = Color.PALE_GREEN
+	print("Hello world")
